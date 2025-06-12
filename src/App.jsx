29d16c6549dc744d93e1928a852af1236898860d
Hem1234567 <hem07042006@gmail.com>
2025-06-13 00:27:32 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import { BackgroundEffects } from "./components/BackgroundEffects";
 import { TitleSection } from "./components/TitleSection";
 import { CTAButtons } from "./components/CTAButtons";
@@ -6,15 +7,21 @@ import { SocialLinks } from "./components/SocialLinks";
 export const App = () => {
   return (
     <BackgroundEffects>
-      <header className="w-full fixed top-0 z-50 bg-transparent ">
+      <header className="w-full fixed top-0 z-50 bg-transparent">
         <nav className="max-w-7xl mx-auto flex items-center justify-between px-4 py-3 sm:py-3">
-          <a href="#" className="flex items-center cursor-default">
+          <motion.a
+            href="#"
+            className="flex items-center cursor-default"
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay: 0.5 }} // Adjust delay as needed
+          >
             <img
               src="/pechacks bgr.png"
               alt="Logo"
-              className="h-6 sm:h-8 lg:ml-2 lg:mt-10"
+              className="h-6 sm:h-8 lg:ml-2 lg:mt-10 hover:scale-110 transition-transform duration-300"
             />
-          </a>
+          </motion.a>
         </nav>
       </header>
 
