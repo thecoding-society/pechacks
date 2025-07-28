e3b8af7a595c9c52bb0a848c163402c20429410e
Alphamoris <alphamoris45@gmail.com>
2025-07-28 21:19:48 +0530

    Solved cors errors and removed manifest linking

--- vite.config.js
+++ vite.config.js
@@ -13,13 +13,33 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: undefined,
+        assetFileNames: (assetInfo) => {
+          // Ensure proper file extensions for assets
+          const info = assetInfo.name.split('.');
+          const ext = info[info.length - 1];
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(ext)) {
+            return `assets/[name]-[hash][extname]`;
+          }
+          if (/css/i.test(ext)) {
+            return `assets/[name]-[hash][extname]`;
+          }
+          return `assets/[name]-[hash][extname]`;
+        },
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
       },
     },
   },
   server: {
     port: 3000,
+    headers: {
+      'Content-Type': 'application/javascript; charset=utf-8'
+    }
   },
   preview: {
     port: 4173,
+    headers: {
+      'Content-Type': 'application/javascript; charset=utf-8'
+    }
   }
 });
